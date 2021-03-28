389b63061442fdbbe994f7d4240d0a54e69a6b50
Balazs David <balazsdavid987@gmail.com>
2021-03-28 13:36:57 +0200

    Add missing key attribute

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,7 +3,7 @@ import styles from "./ContactList.module.scss";
 const ContactList = ({ accounts }) => (
   <div className={styles.component}>
     {accounts.map(({ text, href, icon }) => (
-      <div className={styles.account}>
+      <div key={text} className={styles.account}>
         <img
           className={styles.icon}
           src={`/icons/${icon}.svg`}
