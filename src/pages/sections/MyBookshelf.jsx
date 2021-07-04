bbf45674ea75b1abe47c91cf604bb4080a821b8e
Balazs David <balazsdavid987@gmail.com>
2021-07-04 08:58:26 +0200

    Refactor MyBookshelf

--- src/pages/sections/MyBookshelf.jsx
+++ src/pages/sections/MyBookshelf.jsx
@@ -1,18 +1,26 @@
 import Link from "next/link";
 
+const CurrentlyReading = (currentlyReading) => {
+  if (currentlyReading.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <p>Currently reading: </p>
+      <ul>
+        {currentlyReading.map((title) => (
+          <li key={title}>{title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
 const MyBookshelf = ({ currentlyReading }) => (
   <section>
     <h2>My bookshelf</h2>
-    {currentlyReading.length !== 0 && (
-      <>
-        <p>Currently reading: </p>
-        <ul>
-          {currentlyReading.map((title) => (
-            <li key={title}>{title}</li>
-          ))}
-        </ul>
-      </>
-    )}
+    <CurrentlyReading currentlyReading={currentlyReading} />
     <p>
       <Link href="/bookshelf">Check out my book reviews &rarr;</Link>
     </p>
