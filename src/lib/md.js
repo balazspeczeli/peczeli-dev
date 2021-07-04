fa7fa8183b2b2e74f21112665eae2682520f1af1
Balazs David <balazsdavid987@gmail.com>
2021-07-04 11:01:00 +0200

    Add sytax highlighting for code blocks

--- src/lib/md.js
+++ src/lib/md.js
@@ -1,7 +1,8 @@
 import markdownit from "markdown-it";
+import prism from "markdown-it-prism";
 
 const md = markdownit({
   html: true,
-});
+}).use(prism, {});
 
 export default md;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import "../styles/global.scss";
+import "../styles/prism-vsc-dark-plus.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
