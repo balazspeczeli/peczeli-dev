efb79607d205630d1f2975419b0da4f5105af656
Balazs David <balazsdavid987@gmail.com>
2021-08-19 02:12:18 +0200

    Add config.json

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
-import { BLOG_TITLE } from "../Layout/Layout";
+
 import styles from "./Header.module.scss";
+
+import { blogTitle } from "../../content/config.json";
 
 const Header = () => (
   <header className={styles.component}>
-    <span className={styles.title}>{BLOG_TITLE}</span>
+    <span className={styles.title}>{blogTitle}</span>
     <nav>
       <Link href="/">Home</Link>
     </nav>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,10 +3,10 @@ import Header from "../Header/Header";
 
 import styles from "./Layout.module.scss";
 
-export const BLOG_TITLE = "Balázs Péczeli";
+import { blogTitle } from "../../content/config.json";
 
 const Layout = ({ title, children }) => {
-  const pageTitle = title ? `${title} | ${BLOG_TITLE}` : BLOG_TITLE;
+  const pageTitle = title ? `${title} | ${blogTitle}` : blogTitle;
 
   return (
     <div className={styles.component}>
