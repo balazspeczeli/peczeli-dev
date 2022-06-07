558f7a1a66dec059cf3d461dd8bc1be4bd727fa4
balazspeczeli <balazspeczeli87@gmail.com>
2022-06-07 18:30:15 +0200

    Refactor Header component

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,16 +1,24 @@
+import { Nav } from "./Nav";
 import Link from "next/link";
 
 import styles from "./Header.module.scss";
 
 import config from "content/config.json";
 
-export const Header = () => (
-  <header className={styles.component}>
-    <span className={styles.title}>
-      <Link href="/">{config.blogTitle}</Link>
-    </span>
-    <nav>
-      <Link href="/">Home</Link>
-    </nav>
-  </header>
-);
+export const Header = () => {
+  const navItems = [
+    { href: "/", text: "Home" },
+    { href: "/posts", text: "Posts" },
+    { href: "/my-bookshelf", text: "My bookshelf" },
+    { href: "/about", text: "About" },
+  ];
+
+  return (
+    <header className={styles.component}>
+      <span className={styles.title}>
+        <Link href="/">{config.blogTitle}</Link>
+      </span>
+      <Nav navItems={navItems} />
+    </header>
+  );
+};
---
+++ src/components/Layout/Header/Nav/Nav.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const NavItem = ({ text, href }) => {
+  const [isActive, setIsActive] = useState(false);
+  const { asPath, isReady } = useRouter();
+
+  useEffect(() => {
+    if (isReady) {
+      setIsActive(asPath === href);
+    }
+  }, [asPath, isReady, setIsActive]);
+
+  return (
+    <Link href={href}>
+      <a className={isActive && styles.active}>{text}</a>
+    </Link>
+  );
+};
+
+export const Nav = ({ navItems }) => {
+  return (
+    <nav className={styles.component}>
+      {navItems.map(({ href, text }) => (
+        <NavItem key={href} {...{ href, text }} />
+      ))}
+    </nav>
+  );
+};
