9450f14f7f44a3f8654254df33b54d90c5b7fd8c
balazspeczeli <balazspeczeli87@gmail.com>
2022-06-07 18:22:54 +0200

    Add About page

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,13 +3,16 @@ import styles from "./ContactList.module.scss";
 
 export const ContactList = ({ accounts }) => (
   <div className={styles.component}>
-    {accounts.map(({ text, href, icon }) => (
-      <div key={text} className={styles.account}>
-        <Icon src={`/icons/${icon}.svg`} />
-        <a href={href} target={icon !== "envelope" ? "_blank" : ""}>
-          {text}
-        </a>
-      </div>
-    ))}
+    {Object.keys(accounts).map((account) => {
+      const { text, href, icon } = accounts[account];
+      return (
+        <div key={text} className={styles.account}>
+          <Icon src={`/icons/${icon}.svg`} />
+          <a href={href} target={icon !== "envelope" ? "_blank" : ""}>
+            {text}
+          </a>
+        </div>
+      );
+    })}
   </div>
 );
