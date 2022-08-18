aa88bc44806742350933da58d152dc844d8d9b72
balazspeczeli <balazspeczeli87@gmail.com>
2022-08-18 16:13:54 +0200

    Update About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import { Icon, Layout, HorizontalLine, BackToHome } from "components";
+import { Icon, Layout, HorizontalLine, BackToHome, Polaroid } from "components";
 import accounts from "content/accounts.json";
 
 const AboutPage = () => {
@@ -32,9 +32,32 @@ const AboutPage = () => {
         When not sitting in front of a computer, I enjoy cooking, hiking, and
         spending time with my family.
       </p>
+      <h2>Some photos of me</h2>
+      <Polaroid
+        src="/images/about/photo-1.jpg"
+        caption="In my natural habitat"
+        rotateBy="0.5"
+      />
+      <Polaroid
+        src="/images/about/photo-2.jpg"
+        caption="Moments before dying of exhaustion during a 10k run"
+        rotateBy="-0.5"
+      />
+      <Polaroid
+        src="/images/about/photo-3.jpg"
+        caption="In front of one of my previous workplaces*"
+        rotateBy="0.5"
+      />
       <p>
-        Should you want to send me a message, feel free to reach out to me via
-        email at <a href={accounts.email.href}>{accounts.email.text}</a>.
+        * Taken at{" "}
+        <a
+          href="https://www.google.com/maps/@47.5239994,19.0383778,3a,75y,10.96h,84.19t/data=!3m6!1e1!3m4!1sUPLmJqARfXETwhYGV6u24g!2e0!7i16384!8i8192"
+          target="_blank"
+        >
+          Óbuda Gate
+        </a>
+        when the building was temporarily used for shooting an episode of a
+        television series
       </p>
       <HorizontalLine />
       <BackToHome />
